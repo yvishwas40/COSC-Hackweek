91475263bb23cec920fe66b30bf4225a9b80c0e9
yvishwas40 <yvishwas40@gmail.com>
2025-06-26 23:39:07 +0530

    updated with regression on 2 datasets

--- Regression in R/regression_analysis.R
+++ Regression in R/regression_analysis.R
@@ -1,30 +1,18 @@
-# Load built-in dataset
+# Load datasets
 data(mtcars)
 data(iris)
 
-# View structure
-str(mtcars)
-str(iris)
-
-# Summary statistics
-summary(mtcars)
-summary(iris)
-
-# Base R plot for mtcars
-plot(mtcars$wt, mtcars$mpg, main="MPG vs Weight", xlab="Weight", ylab="MPG")
-
-# Base R plot for iris
-plot(iris$Petal.Length ~ iris$Petal.Width, col=iris$Species)
-
-# Step 2: Build the linear regression model
+# Regression model on mtcars
 model1 <- lm(mpg ~ wt + hp, data = mtcars)
+plot(mtcars$wt, mtcars$mpg, main="MPG vs Weight", xlab="Weight", ylab="MPG")
+abline(lm(mpg ~ wt, data = mtcars), col="red")  # add regression line
 
-# Step 3: Create new data for prediction
-newdata <- data.frame(wt = 3, hp = 110)
+# Regression model on iris
+model2 <- lm(Petal.Length ~ Petal.Width, data = iris)
+plot(iris$Petal.Width, iris$Petal.Length, col=iris$Species,
+     main="Petal Length vs Width", xlab="Petal Width", ylab="Petal Length")
+abline(model2, col="blue", lwd=2)  # add regression line
 
-# Step 4: Predict mpg
-predict(model1, newdata)
-
-# Plot with regression line
-plot(mtcars$wt, mtcars$mpg)
-abline(model1, col="red")
+# Optional prediction
+predict(model1, data.frame(wt=3, hp=110))
+predict(model2, data.frame(Petal.Width=1.5))
